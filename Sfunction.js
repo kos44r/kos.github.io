b22c32a83b598712d0b0a4a478985c58f02d5c0d
kos44r <kosnavan2@icloud.com>
2026-07-12 18:03:44 +1000

    Add files via upload

---
+++ Sfunction.js
@@ -0,0 +1,13 @@
+function openDoor() {
+    window.location.href = "Level9st.html";
+}
+function openDoor2() {
+    window.location.href = "Level9st.html";
+}
+
+
+
+
+
+document.querySelector(".doorknob").onclick = openDoor;
+document.querySelector(".doorknob").onclick = openDoor2;
